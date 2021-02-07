dd51d598ad6cc957c691ec4dc8c5cfbe78774a37
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-07 13:02:34 +0600

    commit

---
+++ Lab 3/Lab 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Student
+    {
+        private string name;
+        private string id;
+        private string department;
+        private float cgpa;
+
+        void ShowInfo()
+        {
+
+        }
+    }
+    class Triangle
+    {
+
+    }
+    class Account
+    {
+
+    }
+    class Course
+}
