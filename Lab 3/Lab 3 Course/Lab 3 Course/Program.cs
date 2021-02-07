a591fcc7a1991cace6aa2d7b50dd764b7b99a963
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-07 13:56:45 +0600

    Course added

---
+++ Lab 3/Lab 3 Course/Lab 3 Course/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_Course
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            String courseName
+            String courseCode
+            int courseCredit
+        }
+    }
+}
