eea00298e7d442e9fc0216843a69e96e96c0605b
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-07 14:04:36 +0600

    Lab 3 Course updated

--- Lab 3/Lab 3 Course/Lab 3 Course/Program.cs
+++ Lab 3/Lab 3 Course/Lab 3 Course/Program.cs
@@ -8,11 +8,23 @@ namespace Lab_3_Course
 {
     class Course
     {
+        public Course()
+        {
+            Console.WriteLine("Constructor called");
+        }
+        private string courseName;
+        private string courseCode;
+        private int courseCredit;
+        void ShowCourseInfo(string courseName, string courseCode, int courseCredit)
+        {
+            Console.WriteLine("Course Name:" + courseName);
+            Console.WriteLine("Course Code:" + courseCode);
+            Console.WriteLine("Course Credit:" + courseCredit);
+        }
         static void Main(string[] args)
         {
-            String courseName
-            String courseCode
-            int courseCredit
+            Course c1 = new Course();
+            c1.ShowCourseInfo("OOP-2", "101", 3);
         }
     }
 }
