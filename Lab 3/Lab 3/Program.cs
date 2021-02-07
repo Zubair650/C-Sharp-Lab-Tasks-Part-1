93651efe7eb2b1548f6d3aab0bbe3369ca75c9eb
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-07 13:33:19 +0600

    Lab 3 updated

--- Lab 3/Lab 3/Program.cs
+++ Lab 3/Lab 3/Program.cs
@@ -6,25 +6,29 @@ using System.Threading.Tasks;
 
 namespace Lab_3
 {
-    class Student
+    public class Student
     {
         private string name;
         private string id;
         private string department;
         private float cgpa;
 
-        void ShowInfo()
+        public Student(string name, string id, string department, float cgpa)
         {
+            Console.WriteLine("Constructor called");
+        }
 
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Department: " + department);
+            Console.WriteLine("Cgpa: " + cgpa);
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Zubair", "101", "CSE", 3.75f);
+            s1.ShowInfo();
         }
     }
-    class Triangle
-    {
-
-    }
-    class Account
-    {
-
-    }
-    class Course
 }
---
+++ Lab 3/Lab 3/Triangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Triangle
+    {
+        private int x = 10;
+        private int y = 10;
+        private int z = 10;
+        void ShowInfo()
+        {
+            Console.WriteLine("1st side: " + x);
+            Console.WriteLine("1st side: " + y);
+            Console.WriteLine("1st side: " + z);
+        }
+        void TestTriangle()
+        {
+            if (x == y && y == z)
+            {
+                Console.WriteLine("Equilateral");
+            }
+            if (x == y || x == z || y == z)
+            {
+                Console.WriteLine("Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("No equal sides");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle();
+            t1.TestTriangle();
+        }
+    }
+}
