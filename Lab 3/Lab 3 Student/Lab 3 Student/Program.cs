b7cdeef8ad0a541e510ed78a769c162e1741c93d
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-07 13:41:21 +0600

    Lab 3 Student added

---
+++ Lab 3/Lab 3 Student/Lab 3 Student/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_Student
+{
+        public class Student
+        {
+            private string name;
+            private string id;
+            private string department;
+            private float cgpa;
+
+            public Student()
+            {
+                Console.WriteLine("Constructor called");
+            }
+
+            public void ShowInfo(string name, string id, string department, float cgpa)
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Department: " + department);
+                Console.WriteLine("Cgpa: " + cgpa);
+            }
+            static void Main(string[] args)
+            {
+                Student s1 = new Student();
+                s1.ShowInfo("Zubair", "101", "CSE", 3.75f);
+            }
+        }
+    }
