0dcf0efd1ac5d1863214e0137bc004d7a548f340
ZUBAIR AHMED <saad03261@gmail.com>
2021-02-14 14:00:01 +0600

    Lab 4 added

---
+++ Lab 4/Duplicate/Duplicate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_Array
+{
+    class Duplicate
+    {
+        static void Main(string[] args)
+        {
+            int[] arr3 = new int[4];
+            arr3[0] = 2;
+            arr3[1] = 2;
+            arr3[2] = 1;
+            arr3[3] = 2;
+            int count = 0;
+            if (arr3[0] == arr3[1] || arr3[0] == arr3[2] || arr3[0] == arr3[3])
+            {
+                count++;
+            }
+            if (arr3[1] == arr3[0] || arr3[1] == arr3[2] || arr3[1] == arr3[3])
+            {
+                count++;
+            }
+            if (arr3[2] == arr3[0] || arr3[2] == arr3[1] || arr3[2] == arr3[3])
+            {
+                count++;
+            }
+            if (arr3[3] == arr3[0] || arr3[3] == arr3[1] || arr3[3] == arr3[2])
+            {
+                count++;
+            }
+            Console.WriteLine("Number of Duplicate elements:" + count);
+
+
+        }
+    }
+}
+
---
+++ Lab 4/Single Dimensional Array/Single Dimensional Array/Copy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_Array
+{
+    class Copy
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            arr[0] = 2;
+            arr[1] = 3;
+            arr[2] = 4;
+            arr[3] = 5;
+            arr[4] = 6;
+            int arrlength = arr.Length;
+            
+                int[] arr2 = new int[arrlength];
+                Array.Copy(arr, arr2, arrlength);
+            
+                foreach (int value in arr2)
+                {
+                    Console.WriteLine("Copied Array:"+ value);
+                }
+            }
+        }
+    }
